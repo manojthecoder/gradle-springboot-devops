4a78e734322972079adb9b5142bf6f4389ef9a95
manoj <manoj@LAPTOP-SH3PG1I0.lan>
2021-08-05 22:22:57 -0500

    Initial Checkin

---
+++ DevOpsGradleSpringboot/src/main/java/com/DevOps/DevOpsGradleSpringboot/DevOpsGradleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.DevOps.DevOpsGradleSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsGradleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsGradleSpringbootApplication.class, args);
+	}
+
+}
---
+++ DevOpsGradleSpringboot/src/test/java/com/DevOps/DevOpsGradleSpringboot/DevOpsGradleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevOps.DevOpsGradleSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsGradleSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
